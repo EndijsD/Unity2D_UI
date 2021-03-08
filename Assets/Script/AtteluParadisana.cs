e450735db3752255a843fcbeec61bb6bc8a1c032
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-03-08 16:37:21 +0200

    Projekts pabeigts

--- Assets/Script/AtteluParadisana.cs
+++ Assets/Script/AtteluParadisana.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class AtteluParadisana : MonoBehaviour
     public GameObject lacitis;
     public GameObject tante;
     public GameObject masina;
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+    public GameObject slider;
+    public AudioSource skanasAvots;
+    public AudioClip skanaKoAtskanot;
 
     public void binaAttelosana(bool vertiba)
     {
@@ -37,5 +43,33 @@ public class AtteluParadisana : MonoBehaviour
     public void paLabiBins()
     {
         bins.transform.localScale = new Vector2(-1, 1);
+    }
+
+    public void izkritosais(int skaitlis)
+    {
+        if(skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        } else if(skaitlis == 1){
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        } else if (skaitlis == 2){
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[2];
+        }
+    }
+
+    public void mainitLielumu()
+    {
+        float pasreizejaVertiba = slider.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector2(1F * pasreizejaVertiba, 1F * pasreizejaVertiba);
+    }
+
+    public void uzbiditsUzAttela()
+    {
+        skanasAvots.PlayOneShot(skanaKoAtskanot);
+    }
+
+    public void kursorsNobidisNoAttela()
+    {
+        skanasAvots.Stop();
     }
 }
---
+++ Assets/Script/ObjektuParvietosana.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Importē, lai lietotu pointer darbību interfeisu
+//Papildus ir jāpārliecinās, ka projektā pie Canvas ir importēta EventSystem UI komponente
+using UnityEngine.EventSystems;
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,  IDragHandler, IEndDragHandler
+{
+    private RectTransform transformacijuLogs;
+    public Canvas kanva;
+
+    private void Awake()
+    {
+        transformacijuLogs = GetComponent<RectTransform>();
+    }
+
+    //Funkcija nostrādā, kad uzklikšķināts uz pārvietojamā objekta
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Kreisais klikšķis uz pārvietojamā objekta!");
+    }
+
+    //Funkcija nostrādā uzsākot pārvietošanas
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsāka pārvietošanu!");
+    }
+
+    //Funkcija nostrādā pārvietošanas brīdī
+    public void OnDrag(PointerEventData notikums)
+    {
+        transformacijuLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        Debug.Log("Vilkšana pabeigta!");
+    }
+}
